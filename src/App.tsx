888c5f6048b78fabb353e8938bed5da787a2c251
xStranged <fede.valle04@gmail.com>
2025-02-04 17:47:53 -0300

    feat: :lipstick: add timePicker

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import { useState } from 'react'
 import './App.css'
+import { Button } from './components/ui/button'
+import { TimePickerDemo } from './components/ui/time-picker-demo'
+import { Input } from './components/ui/input'
 
 function App() {
 
-  const [time, setTime] = useState('')
+  const [time, setTime] = useState<Date | undefined>(undefined)
   const [distance, setDistance] = useState('')
   const [pace, setPace] = useState('')
 
   const calculatePace = () => {
 
-    const timePerKm = time / distance
+    const totalSeconds = (time?.getMinutes() * 60 + time?.getSeconds())
+    const timePerKm = totalSeconds / distance
     const minutes = timePerKm / 60
 
     const decimal = minutes % 1
@@ -18,14 +22,18 @@ function App() {
   }
 
   return (
-    <div className='bg-red-50'>
-      <h1 className=''>SprintJS</h1>
-      <h2>Tiempo total (s)</h2>
-      <input type="text" value={time} onChange={(e) => setTime(e.target.value)} />
-      <h2>Distancia total (km)</h2>
-      <input type="text" value={distance} onChange={(e) => setDistance(e.target.value)} />
-      <br />
-      <button onClick={calculatePace}>Calcular ritmo</button>
+    <div>
+      <h1>SprintJS</h1>
+      <div className='my-2'>
+        <h2>Tiempo total (s)</h2>
+        <TimePickerDemo date={time} setDate={(e) => setTime(e)} />
+      </div>
+      <div className='my-2'>
+        <h2>Distancia total (km)</h2>
+        <Input type='text' value={distance} onChange={(e) => setDistance(e.target.value)} />
+      </div>
+      <Button variant='outline' className='mt-3' onClick={calculatePace}>Calcular ritmo</Button>
+
       <h1>Ritmo: {pace}</h1>
     </div>
   )
