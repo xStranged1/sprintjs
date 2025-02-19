981fb7728ea2d17cae539a4f6edb42e69b999e31
xStranged <fede.valle04@gmail.com>
2025-02-19 12:46:22 -0300

    fix: :wrench: fix api port in production

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,6 +1,8 @@
 const {
     VITE_API_URL = 'http://localhost',
     VITE_API_PORT = '3000',
+    VITE_ENVIRONMENT = 'develop'
 } = import.meta.env;
 
-export const API_URL = `${VITE_API_URL}:${VITE_API_PORT}`
+const PORT = VITE_ENVIRONMENT == 'develop' ? `:${VITE_API_PORT}` : ''
+export const API_URL = `${VITE_API_URL}${PORT}`
--- src/core/Navbar.tsx
+++ src/core/Navbar.tsx
@@ -8,7 +8,7 @@ export function Navbar() {
 
     return (
         <div className="mt-20">
-            {location == "/sprintjs" && (
+            {(location == "/sprintjs/" || location == "/sprintjs") && (
                 <div className="">
                     <img src={iconRunningBlue} className="w-24 justify-self-center mb-2" />
                     <Title title="SprintJS" />
@@ -23,8 +23,8 @@ export function Navbar() {
                 </>
             )}
             <div className="h-10 mt-6 items-center justify-center rounded-md bg-muted p-1 text-muted-foreground mb-10 grid w-full grid-cols-3">
-                <Link className={`${location == '/sprintjs' ? 'bg-background text-foreground' : ''} inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium ring-offset-background transition-all  data-[state=active]:text-foreground data-[state=active]:shadow-sm focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50`}
-                    href="/sprintjs"    >
+                <Link className={`${(location == '/sprintjs/' || location == '/sprintjs') ? 'bg-background text-foreground' : ''} inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium ring-offset-background transition-all  data-[state=active]:text-foreground data-[state=active]:shadow-sm focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50`}
+                    href="/sprintjs/"    >
                     <h3>Home</h3>
                 </Link>
                 <Link href="/stats" className={`${location == '/stats' ? 'bg-background text-foreground' : ''} inline-flex items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5 text-sm font-medium ring-offset-background transition-all  data-[state=active]:text-foreground data-[state=active]:shadow-sm focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50`}>
