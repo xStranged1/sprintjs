f85224e51fab154eed58d05f5ea1862d166b2ff2
xStranged <fede.valle04@gmail.com>
2025-02-19 16:54:41 -0300

    fix: :adhesive_bandage: fix calculate textPace

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -4,15 +4,8 @@ export const getTotalSeconds = (hours: number | undefined, minutes: number | und
 
 export const getPace = (distance: number, hours: number | undefined, minutes: number | undefined, seconds: number | undefined) => {
     const totalSeconds = getTotalSeconds(hours, minutes, seconds)
-    const timePerKm = totalSeconds / (distance / 1000)
-    const minutesPerKm = timePerKm / 60
-    const decimal = minutesPerKm % 1
-    const secondsPerKm = Math.round((decimal * 100) * 60 / 100).toString()
-    let textSeconds = secondsPerKm
-    if (secondsPerKm.length == 1) {
-        textSeconds = secondsPerKm + 0
-    }
-    const textPace = `${Math.floor(minutesPerKm)}:${(textSeconds)}m / KM`
+    const timePerKm = Math.round(totalSeconds / (distance / 1000))
+    const textPace = formatTime(timePerKm)
     return textPace
 }
 
